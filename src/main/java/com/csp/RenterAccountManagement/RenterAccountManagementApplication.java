fad80d3fd6cb6670d19af32bb939955296f870f9
chandra sekhar <chandra.polavarapu516@topper.wku.edu>
2020-12-26 14:59:23 -0700

    initial commit

---
+++ src/main/java/com/csp/RenterAccountManagement/RenterAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.csp.RenterAccountManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenterAccountManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenterAccountManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csp/RenterAccountManagement/RenterAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp.RenterAccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenterAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
